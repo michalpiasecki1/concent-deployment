455e03b9341e89a2be15f950d9484b3db8d5f795
bartoszbetka <bartoszbetka94@gmail.com>
2018-07-18 14:09:00 +0200

    Add kubernetes deployment for middleman

--- kubernetes/delete-config-maps.sh
+++ kubernetes/delete-config-maps.sh
@@ -12,4 +12,5 @@ kubectl delete configmap conductor-worker-settings    || true
 kubectl delete configmap concent-api-settings         || true
 kubectl delete configmap concent-api-worker-settings  || true
 kubectl delete configmap verifier-settings            || true
+kubectl delete configmap middleman-settings           || true
 kubectl delete secret concent-api-secrets             || true
